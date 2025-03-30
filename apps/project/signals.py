3d1556777aac5f06969be92bb9ebf55dabc02114
josecarlospr2002 <josecarlosop23miami@gmail.com>
2025-03-30 13:27:42 -0400

    reactivo consumido automatico

--- apps/project/signals.py
+++ apps/project/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
-from .models import EntradaDeReactivo, Reactivo
+from .models import EntradaDeReactivo, Reactivo, Reactivo_Consumido 
 
 @receiver(pre_save, sender=EntradaDeReactivo)
 def manejar_edicion_entrada_reactivo(sender, instance, **kwargs):
@@ -24,20 +24,39 @@ def manejar_edicion_entrada_reactivo(sender, instance, **kwargs):
         instance.reactivo.cantidad_de_disponible += instance.cantidad_de_reactivo
         instance.reactivo.save()
 
-# @receiver(post_save, sender=EntradaDeReactivo)
-# def actualizar_cantidad_reactivo_al_guardar(sender, instance, created, **kwargs):
-#     """Actualiza la cantidad disponible cuando se crea o edita una entrada"""
-#     reactivo = instance.reactivo
-#     if created:  # Si es una nueva entrada
-#         # Sumamos la nueva cantidad al disponible
-#         reactivo.cantidad_de_disponible += instance.cantidad_de_reactivo
-#         reactivo.save()
-#     # No necesitamos else aquí porque las ediciones se manejan en pre_save
-
 @receiver(post_delete, sender=EntradaDeReactivo)
 def actualizar_cantidad_reactivo_al_borrar(sender, instance, **kwargs):
     """Actualiza la cantidad disponible cuando se elimina una entrada"""
     reactivo = instance.reactivo
     # Restamos la cantidad que se eliminó
     reactivo.cantidad_de_disponible -= instance.cantidad_de_reactivo
+    reactivo.save()
+
+@receiver(pre_save, sender=Reactivo_Consumido)
+def manejar_edicion_reactivo_consumido(sender, instance, **kwargs):
+    """Maneja el caso de edición de un consumo existente"""
+    if instance.pk:  # Si es una edición (no una creación nueva)
+        consumo_anterior = Reactivo_Consumido.objects.get(pk=instance.pk)
+        # Si la nueva cantidad es mayor que la anterior, restamos más del disponible
+        if instance.cantidad_de_reactivo_consumida > consumo_anterior.cantidad_de_reactivo_consumida:
+            diferencia = instance.cantidad_de_reactivo_consumida - consumo_anterior.cantidad_de_reactivo_consumida
+            instance.reactivo.cantidad_de_disponible -= diferencia
+            instance.reactivo.save()
+        else:
+            # Si la nueva cantidad es menor, devolvemos la diferencia al disponible
+            if instance.cantidad_de_reactivo_consumida < consumo_anterior.cantidad_de_reactivo_consumida:
+                diferencia = consumo_anterior.cantidad_de_reactivo_consumida - instance.cantidad_de_reactivo_consumida
+                instance.reactivo.cantidad_de_disponible += diferencia
+                instance.reactivo.save()
+    else:
+        # Es una nueva entrada, restamos del disponible
+        instance.reactivo.cantidad_de_disponible -= instance.cantidad_de_reactivo_consumida
+        instance.reactivo.save()
+
+@receiver(post_delete, sender=Reactivo_Consumido)
+def actualizar_reactivo_consumido_al_borrar(sender, instance, **kwargs):
+    """Actualiza la cantidad disponible cuando se elimina un consumo"""
+    reactivo = instance.reactivo
+    # Al eliminar un consumo, devolvemos la cantidad al disponible
+    reactivo.cantidad_de_disponible += instance.cantidad_de_reactivo_consumida
     reactivo.save()
